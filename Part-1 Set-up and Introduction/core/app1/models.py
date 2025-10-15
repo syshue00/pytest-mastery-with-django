89849a7ac39b9d1230676ba59488353554acbbe1
syshue00 <syshue00@gmail.com>
2025-10-15 17:31:37 -0400

    Part 1 - Setup and Intro

--- Part-1 Set-up and Introduction/core/app1/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class Animal(models.Model):
-    name = models.CharField(max_length=50)
-    sound = models.CharField(max_length=50)
---
+++ tests/test_ex1.py
@@ -0,0 +1,8 @@
+import pytest
+
+@pytest.mark.slow
+def test_example():
+    assert 1 == 1
+
+def test_example1():
+    assert 1 == 1
